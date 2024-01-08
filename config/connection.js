d0a507030022a85ebc539d6ae6280f3d520db9ce
Mahmoud Nabil <mahmoudmuhamednabil@gmail.com>
2024-01-08 03:26:01 -0500

    adding connection file

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+// establishing mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
