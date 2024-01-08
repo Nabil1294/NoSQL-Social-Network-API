1d9b02a776fe75b71af5dc0ec4a061b37dd06db1
Mahmoud Nabil <mahmoudmuhamednabil@gmail.com>
2024-01-08 03:40:49 -0500

    adding routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// Routes for thoughts
+router.route('/')
+  .get(getThoughts)   
+  .post(createThought); 
+
+// Routes for a single thought
+router
+  .route('/:thoughtId')  
+  .get(getSingleThought) 
+  .put(updateThought)   
+  .delete(deleteThought);   
+
+// Routes for reactions to a thought
+router.route('/:thoughtId/reactions')
+  .post(createReaction);  
+
+// Routes for a single reaction to a thought
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);  
+
+module.exports = router;
